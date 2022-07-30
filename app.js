27383d80103c01db3330770c622525b4b0504e86
Хворых Виталий <vithvo@bk.ru>
2022-07-30 16:44:26 +0700

    again

--- app.js
+++ app.js
@@ -1242,8 +1242,90 @@ const lovefun = (Ͼ, Ͽ) => !!((Ͼ & 1) ^ (1 & Ͽ));
 
 function digPow(n, p) {
   let a = [...n.toString()];
-  a.map((i, k) => Math.pow(i, p + k));
-  return a;
+  a = a.map((i, k) => Math.pow(i, p + k));
+  a = a.reduce((a, b) => a + b);
+  let b = a / n;
+  return b % 1 == 0 ? b : -1;
 }
 
-console.log(digPow(89, 1));
+// console.log(digPow(695, 2));
+
+// Best practice
+
+function digPow(n, p) {
+  var x = String(n)
+    .split("")
+    .reduce((s, d, i) => s + Math.pow(d, p + i), 0);
+  return x % n ? -1 : x / n;
+}
+
+// 53. Simple, given a string of words, return the length of the shortest word(s). String will never be empty and you do not need to account for different data types.
+
+function findShort(s) {
+  a = s.split(" ").map((i) => (i = i.length));
+  a.sort((a, b) => a - b);
+  return a[0];
+}
+// console.log(findShort("bitcoin take over the world maybe who knows perhaps"));
+
+// Best practice
+
+function findShort(s) {
+  return Math.min.apply(
+    null,
+    s.split(" ").map((w) => w.length)
+  );
+}
+
+function findShort(s) {
+  return Math.min(...s.split(" ").map((s) => s.length));
+}
+
+// // 54. The wave (known as the Mexican wave in the English-speaking world outside North America) is an example of metachronal rhythm achieved in a packed stadium when successive groups of spectators briefly stand, yell, and raise their arms. Immediately upon stretching to full height, the spectator returns to the usual seated position. The result is a wave of standing spectators that travels through the crowd, even though individual spectators never move away from their seats. In many large arenas the crowd is seated in a contiguous circuit all the way around the sport field, and so the wave is able to travel continuously around the arena; in discontiguous seating arrangements, the wave can instead reflect back and forth through the crowd. When the gap in seating is narrow, the wave can sometimes pass through it. Usually only one wave crest will be present at any given time in an arena, although simultaneous, counter-rotating waves have been produced. (Source Wikipedia)
+// Task
+// In this simple Kata your task is to create a function that turns a string into a Mexican Wave. You will be passed a string and you must return that string in an array where an uppercase letter is a person standing up.
+// Rules
+//  1.  The input string will always be lower case but maybe empty.
+
+//  2.  If the character in the string is whitespace then pass over it as if it was an empty seat
+// Example
+// wave("hello") => ["Hello", "hEllo", "heLlo", "helLo", "hellO"]
+
+function wave(str) {
+  let ind = [];
+  str = str.toLowerCase();
+  str = str.split("").map((i, k) => {
+    if (i == " ") {
+      ind.push(k);
+    }
+    return (i = str);
+  });
+  let arr = str.map((i, k) => {
+    i = i.split("");
+    i.splice(k, 1, i[k].toUpperCase());
+    return (i = i.join(""));
+  });
+  res = arr.filter((_, i) => {
+    return !ind.includes(i);
+  });
+  return res;
+}
+
+
+// Best practice
+
+function wave(str) {
+	let result = [];
+	
+  str.split("").forEach((char, index) => {
+		if (/[a-z]/.test(char)) {
+			result.push(
+				str.slice(0, index) + char.toUpperCase() + str.slice(index + 1)
+				);
+			}
+		});
+		
+		return result;
+	}
+	
+	console.log(wave("Hello HEllo"));
